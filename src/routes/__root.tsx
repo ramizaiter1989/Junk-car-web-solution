a5ca0802347a905ce837a41b01727618ec5f9f3a
ramizeaiter <ramizaiter1989@gmail.com>
2026-06-01 01:10:50 +0200

    google analytics and g tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,6 +20,7 @@ import { QuotePopup } from "@/components/site/QuotePopup";
 import { QuotePopupProvider } from "@/components/site/QuotePopupContext";
 
 const GA_MEASUREMENT_ID = "G-JWK882Q31Z";
+const GTM_CONTAINER_ID = "GTM-N48HKMF5";
 
 function NotFoundComponent() {
   const business = useSiteBusiness();
@@ -144,6 +145,13 @@ export const Route = createRootRouteWithContext<{
     ],
     scripts: [
       {
+        children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`,
+      },
+      {
         src: `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`,
         async: true,
       },
@@ -175,6 +183,15 @@ function RootShell({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <HiddenPartnerLink />
         {children}
         <Scripts />
